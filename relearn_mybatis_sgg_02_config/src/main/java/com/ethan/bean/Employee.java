1c1b7b5c16c97e76470bedb72335b9b93abca557
Ethan <ethansung@foxmail.com>
2021-10-28 00:24:51 +0800

    初始化sgg的前两节的mybatis的学习的代码

---
+++ relearn_mybatis_sgg_02_config/src/main/java/com/ethan/bean/Employee.java
@@ -0,0 +1,11 @@
+package com.ethan.bean;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private Integer id;
+    private String lastName;
+    private String email;
+    private String gender;
+}
