0822a86f12c29785eb1a2776c9620d7dc146f640
Ethan <ethansung@foxmail.com>
2021-11-06 03:51:21 +0800

    <feat>oracle的两种批量插入的方式

--- relearn_mybatis_sgg_04_dynamicSQL/src/main/java/com/ethan/dao/EmployeeDynamicSQLMapper.java
+++ relearn_mybatis_sgg_04_dynamicSQL/src/main/java/com/ethan/dao/EmployeeDynamicSQLMapper.java
@@ -7,12 +7,18 @@ import java.util.List;
 
 public interface EmployeeDynamicSQLMapper {
     /**
-     * mysql下的批量插入的实现方式
+     * mysql下的批量插入的两种实现方式
      * 注解@Param("employeeList")很关键，用于映射foreach标签中的collecion
      * @param employeeList
      * @return
      */
     public int batchInsertEmpsByMysql(@Param("employeeList") List<Employee> employeeList);
 
+    /**
+     * oracle下的批量插入的两种实现方式
+     * @param employeeList
+     * @return
+     */
+    public int batchInsertEmpsByOracle(@Param("employeeList") List<Employee> employeeList);
 
 }
